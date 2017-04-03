d21572e22bf6464cc582d6812ca1e6bc788703bd
Raymond Nepstad <raymond.nepstad@sintef.no>
2017-04-03 10:03:46 +0200

    Add simple datalogger class

---
+++ pysilcam/datalogger.py
@@ -0,0 +1,17 @@
+
+class DataLogger:
+    def __init__(self, filename, header):
+        self.filename = filename
+
+        #Generate file with header
+        with open(filename, 'w') as fh:
+            fh.write(header)
+
+    def append_data(self, data):
+        #Append data line to file
+        with open(filename, 'a') as fh:
+            #Generate data line string for data list
+            line = ','.join([str(el) for el in data])
+
+            #Append line to file
+            fh.write(line + '\n')
