3e18b2fefaaf509f064658ed51cce9694dd22b4a
emlyn <emlyn.davies@sintef.no>
2017-05-25 18:14:02 +0200

    add some DNN classification tools

---
+++ pysilcam/silcam_classify.py
@@ -0,0 +1,58 @@
+  # -*- coding: utf-8 -*-
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+import scipy
+import numpy as np
+
+
+def load_model(MODEL='/mnt/ARRAY/classifier/model/particle-classifier.tfl'):
+    OUTPUTS = 3
+
+    # Same network definition as before
+    img_prep = ImagePreprocessing()
+    img_prep.add_featurewise_zero_center()
+    img_prep.add_featurewise_stdnorm()
+    img_aug = ImageAugmentation()
+    img_aug.add_random_flip_leftright()
+    img_aug.add_random_rotation(max_angle=25.)
+    img_aug.add_random_blur(sigma_max=3.)
+
+    network = input_data(shape=[None, 32, 32, 3],
+                         data_preprocessing=img_prep,
+                         data_augmentation=img_aug)
+    network = conv_2d(network, 32, 3, activation='relu')
+    network = max_pool_2d(network, 2)
+    network = conv_2d(network, 64, 3, activation='relu')
+    network = conv_2d(network, 64, 3, activation='relu')
+    network = max_pool_2d(network, 2)
+    network = fully_connected(network, 512, activation='relu')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, OUTPUTS, activation='softmax')
+    network = regression(network, optimizer='adam',
+                         loss='categorical_crossentropy',
+                         learning_rate=0.001)
+
+    model = tflearn.DNN(network, tensorboard_verbose=0, checkpoint_path=MODEL)
+    model.load(MODEL)
+
+    return model
+
+def predict(img, model):
+    # Load the image file
+    #img = scipy.ndimage.imread(args.image, mode="RGB")
+
+    # Scale it to 32x32
+    img = scipy.misc.imresize(img, (32, 32), interp="bicubic").astype(np.float32, casting='unsafe')
+
+    # Predict
+    prediction = model.predict([img])
+
+    #print('other,  copepod,  diatom chain')
+    #print(prediction)
+    return prediction
+
+
---
+++ tfl_tools/investigate_particles.py
@@ -0,0 +1,74 @@
+import pysilcam.process as scpr
+import pysilcam.postprocess as scpp
+import pysilcam.plotting as scplt
+from pysilcam.config import load_config, PySilcamSettings
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import skimage.io as skiio
+import skimage
+import seaborn as sns
+sns.set_style('ticks')
+
+#os.chdir('/mnt/PDrive/ENTICE/notebooks/')
+#print(os.getcwd())
+
+import sys
+sys.path.insert(0, '/mnt/PDrive/ENTICE/notebooks/')
+
+from entice import *
+
+def particle_generator():
+
+
+    print('load ctd')
+    ctd_all = load_ctd()
+    print(' ok')
+
+    stn = 'STN12'
+    mindepth = 1
+    maxdepth = 70
+
+    config_file = '/mnt/ARRAY/ENTICE/Data/configs/config.ini'
+    stats_csv_file = '/mnt/ARRAY/ENTICE/Data/proc/' + stn + '-STATS.csv'
+
+    time = ctd_all['Date_Time']
+    depth = ctd_all['Depth']
+
+    conf = load_config(config_file)
+    settings = PySilcamSettings(conf)
+
+    stats = pd.read_csv(stats_csv_file)
+
+    stats = scpp.add_depth_to_stats(stats, time, depth)
+    print('all stats:', len(stats))
+
+    sstats = stats[(stats['Depth']>mindepth) & (stats['Depth']<maxdepth)]
+    print('selected stats:', len(sstats))
+
+
+    index = 0
+
+    while True:
+
+#        if np.random.choice([0,1]):
+        sstats_ = scpp.extract_nth_largest(sstats,settings,n=index)
+#        else:
+#            sstats_ = scpp.extract_nth_longest(sstats,settings,n=index)
+        print(sstats_)
+
+        filename = os.path.join('/mnt/ARRAY/ENTICE/Data/export/',
+            sstats_['export name'])
+
+        im = skiio.imread(filename)
+
+        im = scpp.explode_contrast(im)
+        im = scpp.bright_norm(im)
+        # scplt.show_imc(im)
+        # plt.title(selected_stats['export name'] + ('\nDepth:
+        # {0:.1f}'.format(selected_stats['depth'])) + 'm\n')
+
+        index += 1
+
+        yield im
