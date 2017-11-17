9dbb3ae913d67f7bee394a0478d78218d64a0138
Emlyn Davies <emlyn.davies@sintef.no>
2017-11-17 12:52:00 +0100

    implement alternative background shifting. Needs testing.

--- pysilcam/background.py
+++ pysilcam/background.py
@@ -34,10 +34,12 @@ def shift_bgstack(bgstack, imbg, imnew):
       imbg (updated actual background image)
     '''
     imold = bgstack.pop(0)  # pop the oldest image from the stack,
-    #imbg -= (imold/len(bgstack))
-    #imbg += (imnew/len(bgstack))
+    # subtract the old image from the average (scaled by the average window)
+    imbg -= (imold/len(bgstack))
+    # add the new image to the average (scaled by the average window)
+    imbg += (imnew/len(bgstack))
     bgstack.append(imnew)  # append the new image to the stack
-    imbg = np.mean(bgstack, axis=0)
+    #imbg = np.mean(bgstack, axis=0)
     return bgstack, imbg
 
 
