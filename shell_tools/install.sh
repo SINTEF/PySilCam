ceb0b64d4def992b2ea81de5e62eaf0b45d664f3
emlyn <emlyn.davies@sintef.no>
2018-08-13 13:42:48 +0200

    update install.sg

--- shell_tools/install.sh
+++ shell_tools/install.sh
@@ -9,16 +9,16 @@ sudo apt-get install -y git wget
 #wget https://repo.anaconda.com/archive/Anaconda3-5.1.0-Linux-x86_64.sh
 #bash Anaconda3-5.1.0-Linux-x86_64.sh
 
-conda update --yes -n base conda
-yes | pip install --upgrade pip
+#conda update --yes -n base conda
+#yes | pip install --upgrade pip
 
 ## pymba installation
 #RUN pip install git+https://github.com/mabl/pymba@python3
 
 # pysilcam requirements
 conda install --yes -c cogsci pygame psutil sphinx sphinxcontrib pyserial seaborn pyserial setuptools
-conda install --yes tensorflow==1.1.0 -c conda-forge
-yes | pip install tflearn h5py cmocean psutil openpyxl Sphinx sphinxcontrib-napoleon
+#conda install --yes tensorflow==1.1.0 -c conda-forge
+yes | pip install tensorflow tflearn h5py cmocean psutil openpyxl Sphinx sphinxcontrib-napoleon
 
 python setup.py develop
 python setup.py test
