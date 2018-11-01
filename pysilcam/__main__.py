fecb2636927f04b00649c6bd94d9cc42166aba97
Raymond Nepstad <raymond.nepstad@sintef.no>
2018-11-01 16:06:00 +0100

    PySilCam ascii art renders correctly on command line

--- pysilcam/__main__.py
+++ pysilcam/__main__.py
@@ -24,7 +24,7 @@ import pandas as pd
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 
-title = '''
+title = r'''
  ____        ____  _ _  ____
 |  _ \ _   _/ ___|(_) |/ ___|__ _ _ __ ___
 | |_) | | | \___ \| | | |   / _` | '_ ` _ \
