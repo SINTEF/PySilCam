c93305cecb39681513036ec9d57775e7e0f00d10
Bjarne Kvæstad <bjarne.kvaestad@sintef.no>
2018-12-14 18:05:46 +0100

    New test script for video stream from camera

---
+++ pysilcam/asyncFrameGrab.py
@@ -0,0 +1,84 @@
+from pymba import *
+import time
+import queue
+import numpy as np
+from pysilcam.config import load_camera_config
+import skimage
+import os
+from cv2 import cvtColor, COLOR_BAYER_BG2RGB, imwrite
+import cv2
+import threading
+import colour
+from colour.plotting import *
+
+from colour_demosaicing import (
+    EXAMPLES_RESOURCES_DIRECTORY,
+    demosaicing_CFA_Bayer_bilinear,
+    demosaicing_CFA_Bayer_Malvar2004,
+    demosaicing_CFA_Bayer_Menon2007,
+    mosaicing_CFA_Bayer)
+
+OETF = colour.RGB_COLOURSPACES['sRGB'].encoding_cctf
+
+def saveImg(img, i):
+    path = '/home/bjarne/data/silcam_files/temp/'
+    img = cvtColor(img, COLOR_BAYER_BG2RGB)
+    np.save(path + str(1), img, allow_pickle=False)
+
+    print('Frequency: ', 1/(time.time() - saveImg.t))
+    saveImg.t = time.time()
+saveImg.t = time.time()
+
+def frameDone_callback(frame):
+    frame.queueFrameCapture(frameCallback=frameDone_callback)
+    img = np.ndarray(buffer=frame0.getBufferByteData(), dtype=np.uint8, shape=(frame0.height, frame0.width))
+    imQueue.put(img)
+
+imQueue = queue.LifoQueue(10)
+
+# start Vimba
+with Vimba() as vimba:
+
+    # INTI
+
+    # get system object
+    system = vimba.getSystem()
+
+    # list available cameras (after enabling discovery for GigE cameras)
+    if system.GeVTLIsPresent:
+        system.runFeatureCommand("GeVDiscoveryAllOnce")
+        time.sleep(0.2)
+    cameraIds = vimba.getCameraIds()
+    for cameraId in cameraIds:
+        print(cameraId)
+
+    # get and open a camera
+    camera = vimba.getCamera(cameraIds[0])
+    camera.openCamera()
+
+    #INIT Done
+    #config
+
+    # Read the configiration values from default config file
+    configPath = 'camera_config_defaults.ini'
+    config = load_camera_config(configPath)
+    config = load_camera_config('', config)
+
+    #If a config is specified, override those values
+    for k, v in config.items():
+        #print(k,'=',v)
+        setattr(camera, k, v)
+
+    camera.startCapture()
+
+    frame0 = camera.getFrame()
+    frame0.announceFrame()
+
+    frame0.queueFrameCapture(frameCallback=frameDone_callback)
+    camera.runFeatureCommand('AcquisitionStart')
+
+
+    while(1):
+        img = imQueue.get()
+        print(imQueue.qsize())
+        saveImg(img, 1)
--- pysilcam/syncFrameGrab.py
+++ pysilcam/syncFrameGrab.py
@@ -4,17 +4,21 @@ import numpy as np
 from pysilcam.config import load_camera_config
 import skimage
 import os
+from cv2 import cvtColor, COLOR_BAYER_BG2RGB, imwrite
 import cv2
 import threading
 
-
 def saveImg(img, i):
     path = '/home/bjarne/data/silcam_files/temp/'
-    #cv2.imwrite(path + str(i) + 'bayer.bmp', img)
-    #print('bayer: Sum: ', img.sum(), 'shape: ', img.shape)
-    img = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2RGB)
-    print(str(i), ' Sum: ', img.sum(), 'shape: ', img.shape)
-    cv2.imwrite(path + str(i) + 'bgr.bmp', img)
+    #e1 = cv2.getTickCount()
+    img = cvtColor(img, COLOR_BAYER_BG2RGB)
+    #e2 = cv2.getTickCount()
+
+    #t = (e2 - e1)/cv2.getTickFrequency()
+    #print(t)
+    #print(str(i), ' Sum: ', img.sum(), 'shape: ', img.shape)
+    np.save(path + str(i), img, allow_pickle=False)
+
 
 # start Vimba
 with Vimba() as vimba:
@@ -50,7 +54,7 @@ with Vimba() as vimba:
 
     #If a config is specified, override those values
     for k, v in config.items():
-        print(k,'=',v)
+        #print(k,'=',v)
         setattr(camera, k, v)
 
     # list camera features
@@ -65,42 +69,42 @@ with Vimba() as vimba:
     #camera0.AcquisitionMode = 'SingleFrame'
 
     start = time.time()
-    samples = 500
+    samples = 100
     imgArr = np.array([])
 
-    threadingSave = True
+    frame0 = camera.getFrame()
+    frame0.announceFrame()
+    camera.startCapture()
+
 
     for i in range(samples):
         print('grabbing frame {}'.format(i))
-        frame0 = camera.getFrame()
-        frame0.announceFrame()
-        #frame0.waitFrameCapture()
 
-
-        camera.startCapture()
         frame0.queueFrameCapture()
         camera.runFeatureCommand('AcquisitionStart')
-        camera.runFeatureCommand('AcquisitionStop')
-        frame0.waitFrameCapture()
-
-        img = np.ndarray(buffer=frame0.getBufferByteData(), dtype=np.uint8, shape=(frame0.height, frame0.width))
-        #img = np.ndarray(buffer=frame0.getBufferByteData(), dtype=np.uint8, shape=(frame0.height, frame0.width, 3))
-        #img = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2BGR)
-        #imgArr = np.append(imgArr, img.copy())
-
-
-        if threadingSave:
-            t = threading.Thread(target=saveImg, args=(img.copy(),i))
-            t.daemon = True
-            t.start()
+        err = frame0.waitFrameCapture(timeout=500)
+        if err == 0:
+            img = np.ndarray(buffer=frame0.getBufferByteData(), dtype=np.uint8, shape=(frame0.height, frame0.width))
+            #saveImg(img, i)
         else:
-            saveImg(img.copy(), i)
+            print(err)
+            camera.revokeAllFrames()
+            frame0 = camera.getFrame()
+            frame0.announceFrame()
 
-        # clean up after capture
-        camera.endCapture()
-        camera.revokeAllFrames()
+        #t = threading.Thread(target=saveImg, args=(img.copy(), i))
+        #t.daemon = True
+        #t.start()
+
+
+        #camera.revokeAllFrames()
+
+
 
     print('acquire frequency: ',samples/(time.time() - start))
+    # clean up after capture
+    camera.endCapture()
+
 
     #start = time.time()
     #for i in range(samples):
