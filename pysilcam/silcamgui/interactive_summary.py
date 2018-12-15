324ee00d69d8f7cb8ef02f4177289f12ffd81184
Emlyn John Davies <emlyn.davies@sintef.no>
2018-12-15 20:55:08 +0100

    SIL-142 include stummary text

--- pysilcam/silcamgui/interactive_summary.py
+++ pysilcam/silcamgui/interactive_summary.py
@@ -38,6 +38,9 @@ class InteractivePlotter(QMainWindow):
         self.setWindowTitle(self.fft_frame.graph_view.stats_filename)
         QApplication.processEvents()
 
+        self.fft_frame.graph_view.update_plot(self.fft_frame.graph_view.mid_time)
+        QApplication.processEvents()
+
 
 class FftFrame(QtWidgets.QFrame):
     def __init__(self, parent=None):
@@ -54,7 +57,13 @@ class GraphView(QtWidgets.QWidget):
     def __init__(self, parent = None):
         super(GraphView, self).__init__(parent)
 
-        self.fig, self.axes = plt.subplots(1,2)
+        # self.fig, self.axes = plt.subplots(2,2)
+        self.fig = plt.figure()
+        self.axisconstant = plt.subplot(221)
+        self.axispsd = plt.subplot(122)
+        self.axistext = plt.subplot(223)
+        plt.sca(self.axistext)
+        plt.axis('off')
         self.canvas = FigureCanvas(self.fig)
         self.canvas.setParent(self)
 
@@ -63,12 +72,9 @@ class GraphView(QtWidgets.QWidget):
         self.layout.setStretchFactor(self.canvas, 1)
         self.setLayout(self.layout)
 
-        plt.sca(self.axes[0])
         self.configfile = "E:/PJ/MiniTowerSilCamConfig.ini"
         self.stats_filename = "E:/PJ/Oseberg2017OilOnly0.25mmNozzle2-STATS.csv"
         self.canvas.draw()
-        # self.setup_figure(self.configfile, self.stats_filename)
-
 
     def setup_figure(self, config_file, stats_csv_file):
         settings = PySilcamSettings(config_file)
@@ -122,15 +128,18 @@ class GraphView(QtWidgets.QWidget):
 
         # f, self.a = plt.subplots(1, 2, figsize=(15, 6))
 
+        plt.sca(self.axisconstant)
         plt.pcolormesh(u, dias, np.log(vd_total.T), cmap=cmocean.cm.amp)
         plt.plot(u, d50_total, 'kx', markersize=5, alpha=0.25)
         plt.plot(u, d50_gas, 'bx', markersize=5, alpha=0.25)
         plt.yscale('log')
+        plt.ylabel('ECD [um]')
 
-        start_time = min(u)
-        end_time = max(u)
-        self.line1 = plt.vlines(start_time, 1, 12000, 'r')
-        self.line2 = plt.vlines(end_time, 1, 12000, 'r')
+        self.start_time = min(u)
+        self.end_time = max(u)
+        self.mid_time = min(u) + (max(u) - min(u)) / 2
+        self.line1 = plt.vlines(self.start_time, 1, 12000, 'r')
+        self.line2 = plt.vlines(self.end_time, 1, 12000, 'r')
 
         self.u = u
         self.vd_total = vd_total
@@ -142,67 +151,80 @@ class GraphView(QtWidgets.QWidget):
     def on_click(self, event):
         if event.inaxes is not None:
             mid_time = pd.to_datetime(matplotlib.dates.num2date(event.xdata))
-            mid_time.tz_convert(None)
-            print(mid_time)
-
-            # mid_time = pd.to_datetime('2018-11-21 11:10:00')
-
-            av_window = pd.to_timedelta('00:00:05')
-
-            start_time = mid_time - av_window / 2
-            end_time = mid_time + av_window / 2
-            u = pd.to_datetime(self.u)
-            print(u[0], start_time, end_time)
-            timeind = np.argwhere((u > start_time.tz_convert(None)) & (u < end_time.tz_convert(None)))
-
-            psd_total = np.mean(self.vd_total[timeind, :], axis=0)[0]
-            psd_oil = np.mean(self.vd_oil[timeind, :], axis=0)[0]
-            psd_gas = np.mean(self.vd_gas[timeind, :], axis=0)[0]
-
-            psd_vc_total = np.sum(psd_total)
-            psd_vc_oil = np.sum(psd_oil)
-            psd_vc_gas = np.sum(psd_gas)
-
-            psd_d50_total = scpp.d50_from_vd(psd_total, self.dias)
-            psd_d50_oil = scpp.d50_from_vd(psd_oil, self.dias)
-            psd_d50_gas = scpp.d50_from_vd(psd_gas, self.dias)
-
-            psd_gor = sum(psd_gas) / (sum(psd_oil) + sum(psd_gas)) * 100
-
-            psd_nims = len(timeind)
-            psd_start = min(u[timeind])
-            psd_end = max(u[timeind])
-            psd_mid = psd_start + (psd_end - psd_start) / 2
-
-
-            plt.sca(self.axes[1])
-            plt.cla()
-            plt.plot(self.dias, psd_total, 'k', linewidth=5)
-            plt.plot(self.dias, psd_oil, color=[0.7, 0.4, 0])
-            plt.plot(self.dias, psd_gas, 'b')
-            plt.xscale('log')
-
-            print('GOR [%]:', psd_gor)
-            print('d50 total [um]:', psd_d50_total)
-            print('d50 oil [um]:', psd_d50_oil)
-            print('d50 gas [um]:', psd_d50_gas)
-            print('VC total [uL/L]:', psd_vc_total)
-            print('VC oil [uL/L]:', psd_vc_oil)
-            print('VC gas [uL/L]:', psd_vc_gas)
-            print('Number of images:', psd_nims)
-            print('start:', pd.to_datetime(psd_start[0]))
-            print('end:', pd.to_datetime(psd_end[0]))
-            print('mid(ish):', pd.to_datetime(psd_mid[0]))
-            print('specified mid time:', mid_time)
-
-            plt.sca(self.axes[0])
-            self.line1.remove()
-            self.line2.remove()
-            self.line1 = plt.vlines(start_time, 1, 12000, 'r')
-            self.line2 = plt.vlines(end_time, 1, 12000, 'r')
-            self.canvas.draw()
+            # mid_time.tz_convert(None)
+            self.update_plot(mid_time)
         else:
             print('Clicked ouside axes bounds but inside plot window')
+
+    def update_plot(self, mid_time):
+
+
+        print(mid_time)
+
+        # mid_time = pd.to_datetime('2018-11-21 11:10:00')
+
+        av_window = pd.to_timedelta('00:00:05')
+
+        start_time = mid_time - av_window / 2
+        end_time = mid_time + av_window / 2
+        u = pd.to_datetime(self.u)
+        print(u[0], start_time, end_time)
+        timeind = np.argwhere((u > start_time) & (u < end_time))
+
+        psd_total = np.mean(self.vd_total[timeind, :], axis=0)[0]
+        psd_oil = np.mean(self.vd_oil[timeind, :], axis=0)[0]
+        psd_gas = np.mean(self.vd_gas[timeind, :], axis=0)[0]
+
+        psd_vc_total = np.sum(psd_total)
+        psd_vc_oil = np.sum(psd_oil)
+        psd_vc_gas = np.sum(psd_gas)
+
+        psd_d50_total = scpp.d50_from_vd(psd_total, self.dias)
+        psd_d50_oil = scpp.d50_from_vd(psd_oil, self.dias)
+        psd_d50_gas = scpp.d50_from_vd(psd_gas, self.dias)
+
+        psd_gor = sum(psd_gas) / (sum(psd_oil) + sum(psd_gas)) * 100
+
+        psd_nims = len(timeind)
+        psd_start = min(u[timeind])
+        psd_end = max(u[timeind])
+        psd_mid = psd_start + (psd_end - psd_start) / 2
+
+
+        plt.sca(self.axispsd)
+        plt.cla()
+        plt.plot(self.dias, psd_total, 'k', linewidth=5, label='Total')
+        plt.plot(self.dias, psd_oil, color=[0.7, 0.4, 0], label='Oil')
+        plt.plot(self.dias, psd_gas, 'b', label='Gas')
+        plt.xlabel('ECD [um]')
+        plt.ylabel('VD [uL/L]')
+        plt.xscale('log')
+
+        plt.sca(self.axistext)
+
+        string = ''
+        string += 'GOR: {:0.01f}'.format(psd_gor)
+        string += '\n d50 total [um]: {:0.0f}'.format(psd_d50_total)
+        string += '\n d50 oil [um]: {:0.0f}'.format(psd_d50_oil)
+        string += '\n d50 gas [um]: {:0.0f}'.format(psd_d50_gas)
+        string += '\n VC total [uL/L]: {:0.0f}'.format(psd_vc_total)
+        string += '\n VC oil [uL/L]: {:0.0f}'.format(psd_vc_oil)
+        string += '\n VC gas [uL/L]: {:0.0f}'.format(psd_vc_gas)
+        string += '\n Num images: {:0.0f}'.format(psd_nims)
+        string += '\n Start: ' + str(pd.to_datetime(psd_start[0]))
+        string += '\n Start: ' + str(pd.to_datetime(psd_start[0]))
+        string += '\n End: ' + str(pd.to_datetime(psd_end[0]))
+        string += '\n Window [sec.] {:0.0f}:'.format(pd.to_timedelta(psd_end[0]-psd_start[0]).seconds)
+
+        plt.title(string, verticalalignment='top', horizontalalignment='right', loc='right')
+
+        plt.sca(self.axisconstant)
+        self.line1.remove()
+        self.line2.remove()
+        self.line1 = plt.vlines(start_time, 1, 12000, 'r')
+        self.line2 = plt.vlines(end_time, 1, 12000, 'r')
+        self.canvas.draw()
+
 
 
 if __name__ == "__main__":
