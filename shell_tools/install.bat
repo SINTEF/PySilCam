3dfaf339da08adf7af153b19ce87d732d37149e4
emlyn <emlyn.davies@sintef.no>
2019-01-24 10:47:57 +0100

    SIL-176 update install for python 35

--- shell_tools/install.bat
+++ shell_tools/install.bat
@@ -1,4 +1,5 @@
 rem install conda packages
+call conda install python==3.5
 call conda install --yes opencv=3.4.2
 call conda install --yes pytables=3.4.2
 
--- shell_tools/update.bat
+++ shell_tools/update.bat
@@ -1,6 +1,2 @@
-cd E:\emlynd\pysilcam-develop\pysilcam\pysilcam
-
-git fetch
-git checkout SIL-108-bsee-ohmsett-working-branch
 git pull
 pause
