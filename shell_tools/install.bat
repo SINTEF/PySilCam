e87cccb46ebbdad9d01ccea2a98586edaa79459a
Emlyn John Davies <emlyn.davies@sintef.no>
2019-10-17 16:32:12 +0200

    SIL-202 fix install.bat so all lines run

--- shell_tools/install.bat
+++ shell_tools/install.bat
@@ -1,7 +1,7 @@
 cd ..\
-conda env create --force environment.yml
-activate silcam
-python setup.py develop
+conda env create --force --yes environment.yml & ^
+activate silcam & ^
+python setup.py develop & ^
 python setup.py test
 
 
