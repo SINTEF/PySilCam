f1abd1c569a26e8f0989cc490641770469d9ab97
William Naylor <william.naylor@sintef.no>
2020-04-29 11:33:50 +0200

    TEMP change to get tests to run in OSX

--- pysilcam/__main__.py
+++ pysilcam/__main__.py
@@ -573,7 +573,7 @@ def collector(inputQueue, outputQueue, datafilename, proc_list, testInputQueue,
 
     countProcessFinished = 0
 
-    while ((outputQueue.qsize() > 0) or (testInputQueue and inputQueue.qsize() > 0)):
+    while ((not outputQueue.empty()) or (testInputQueue and not inputQueue.empty())):
 
         task = outputQueue.get()
 
