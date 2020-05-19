cb762db4867e88e550096a3ea2c7102381480a94
William Naylor <william.naylor@sintef.no>
2020-05-19 15:11:52 +0200

    adding the first draft of the files for the torch version of the tf code

---
+++ torch_tools/ml_config.py
@@ -0,0 +1,12 @@
+random_state = 42
+batch_size = 64
+start_lr = 0.0001
+
+image_size = 32
+# rgb_means = (0.4914, 0.4822, 0.4465)
+# rgb_stds = (0.2023, 0.1994, 0.2010)
+rgb_means = (0.5, 0.5, 0.5)
+rgb_stds = (0.5, 0.5, 0.5)
+
+aug_max_angle = 25.
+aug_blur_max = 3.
