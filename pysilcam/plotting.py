4562909405e83e9216ff10ba9387adcd2ae9ef70
Emlyn Davies <emlyn.davies@sintef.no>
2020-10-28 13:41:06 +0100

    remove unneeded prints in montage_plot

--- pysilcam/plotting.py
+++ pysilcam/plotting.py
@@ -217,11 +217,8 @@ def montage_plot(montage, pixel_size):
         pixel_size (float) : the pixel size of the SilCam used, obtained from settings.PostProcess.pix_size in the
                              config ini file
     '''
-    print('!! Montage shape:', np.shape(montage))
     msize = np.shape(montage)[0]
-    print('!! msize:', msize)
     ex = pixel_size * np.float64(msize) / 1000.
-    print('!! ex:', ex)
 
     ax = plt.gca()
     ax.imshow(montage, extent=[0, ex, 0, ex])
