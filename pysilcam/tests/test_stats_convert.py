d1902ee09cddb46b8e75aee346105d9a624d9adc
Emlyn Davies <emlyn.davies@sintef.no>
2020-12-11 11:16:04 +0100

    update test for correct filenames

--- pysilcam/tests/test_stats_convert.py
+++ pysilcam/tests/test_stats_convert.py
@@ -11,11 +11,15 @@ def test_stats_convert():
 
     with tempfile.TemporaryDirectory() as tempdir:
         print('tempdir:', tempdir, 'created')
-        local_filename = 'STN07-STATS.csv'
-        stats.to_csv(local_filename)
-        statscsv_to_statshdf(local_filename)
-        assert os.path.isfile(local_filename), ('STATS.h5 file not created.')
+        local_csv_filename = 'STN07-STATS.csv'
+        stats.to_csv(local_csv_filename)
+        statscsv_to_statshdf(local_csv_filename)
+        local_hdf_filename = local_csv_filename.replace('-STATS.csv', '-STATS.h5')
+        print(local_hdf_filename)
+        assert os.path.isfile(local_hdf_filename), ('STATS.h5 file not created.')
 
-        stats = pd.read_hdf(local_filename, 'ParticleStats/stats')
+        stats = pd.read_hdf(local_hdf_filename, 'ParticleStats/stats')
         new_stats_shape = np.shape(stats)
+        print('original_stats_shape', original_stats_shape)
+        print('new_stats_shape', new_stats_shape)
         assert (original_stats_shape == new_stats_shape), ('mismatch in data between csv and h5 stats')
