8a4509f739badfeed2a4f4717d0ef7968daa9a38
Emlyn Davies <emlyn.davies@sintef.no>
2021-04-29 12:34:30 +0200

    assume miniforge installed for all users

--- shell_tools/launch.bat
+++ shell_tools/launch.bat
@@ -1,4 +1,4 @@
-set expected_miniconda_path=%UserProfile%\Miniconda3\Scripts\activate.bat
-set silcam_env_path=%conda_path%..\..\..\envs\silcam
-call %conda_path% %silcam_env_path%
+call cd C:\ProgramData\miniforge3\Scripts\activate.bat %UserProfile%\.conda\envs\silcam
+call cd %UserProfile%\Desktop
+call silcam-gui
 pause
